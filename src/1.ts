c72dbca1e4b865541d933ae3f6f5786e386829f5
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-01-07 18:06:19 +0200

    Done 1 and 2

--- src/1.ts
+++ src/1.ts
@@ -2,18 +2,16 @@
   Клас Student, який містить три властивості: name, age та grade. 
   Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
   напишіть скорочену ініціалізацію.
+
+}
+
+export {};
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
+export {};
 
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+class Student {
+  constructor(public name: string, public age: number, public grade: string) {}
 }
 
 export {};
